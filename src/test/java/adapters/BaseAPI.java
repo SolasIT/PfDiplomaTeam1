443a6b56b7ca7dcc5762280fd98115db2e7fc823
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-08-13 21:39:03 +0300

    Added AuthAPI and BaseAPI from master

---
+++ src/test/java/adapters/BaseAPI.java
@@ -0,0 +1,20 @@
+package adapters;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.FilterableRequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseAPI {
+
+    protected static final String BASE_URI = "http://82.142.167.37:4879";
+    static Gson gson = new GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+
+    static FilterableRequestSpecification spec = (FilterableRequestSpecification)
+            given()
+                    .contentType(ContentType.JSON);
+}
